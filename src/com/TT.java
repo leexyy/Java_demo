d07e42594679cd19488b45226d6968b081a9a67c
lihua9475 <1296539475@qq.com>
2018-06-12 15:03:18 +0800

    20180612

--- src/com/TT.java
+++ src/com/TT.java
@@ -1,9 +1,13 @@
 package com;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * @author lihua
  */
 public class TT {
-    //
-    String str = "hello";
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println(str);
+    }
 }
