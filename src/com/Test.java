80349ecc9239ffdec7ae54ba54af2b80dda2563d
lihua9475 <1296539475@qq.com>
2018-06-12 11:47:27 +0800

    20180612

---
+++ src/com/Test.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * Created by Lh on 2018/6/12.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        //
+        System.out.println("Hello World Keep Coding!!!");
+
+    }
+}
---
+++ src/testb/TB.java
@@ -0,0 +1,7 @@
+package testb;
+
+/**
+ * Created by DELL on 2018/6/12.
+ */
+public class TB {
+}
