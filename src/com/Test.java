915f5c0151b7312c0741e8ee9057562aa5ce49d7
lihua9475 <1296539475@qq.com>
2018-06-14 10:08:15 +0800

    测试.gitignore

--- src/com/Test.java
+++ src/com/Test.java
@@ -9,7 +9,7 @@ public class Test {
 
     public static void main(String[] args) {
         //
-        System.out.println("Hello World Keep Coding!!!");
+        System.out.println("Hello World !!!");
         int i = 1;
         //
         Hello hello = new Hello();
